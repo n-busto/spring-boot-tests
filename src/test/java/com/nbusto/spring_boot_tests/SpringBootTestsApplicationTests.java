08431c2379e164c2215528ada31f38755facd551
n-busto <nicanor.manuel.busto@gmail.com>
2024-09-24 13:35:34 +0200

    Initial commit

---
+++ src/test/java/com/nbusto/spring_boot_tests/SpringBootTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbusto.spring_boot_tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
