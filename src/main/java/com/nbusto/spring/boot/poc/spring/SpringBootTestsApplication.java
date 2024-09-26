fd58808ee798c40699f338903186eba93e01ae04
n-busto <nicanor.manuel.busto@gmail.com>
2024-09-26 17:16:34 +0200

    Update spring config

--- src/main/java/com/nbusto/spring/boot/poc/spring/SpringBootTestsApplication.java
+++ src/main/java/com/nbusto/spring/boot/poc/spring/SpringBootTestsApplication.java
@@ -3,7 +3,12 @@ package com.nbusto.spring.boot.poc.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+  scanBasePackages = {
+    "com.nbusto.spring.boot.poc.infra",
+    "com.nbusto.spring.boot.poc.spring"
+  }
+)
 public class SpringBootTestsApplication {
 
   public static void main(String[] args) {
--- src/test/java/com/nbusto/spring/boot/poc/arch/CommonArchitectureRulesTest.java
+++ src/test/java/com/nbusto/spring/boot/poc/arch/CommonArchitectureRulesTest.java
@@ -5,6 +5,10 @@ import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 
@@ -20,8 +24,17 @@ class CommonArchitectureRulesTest {
     .andShould().haveSimpleNameNotEndingWith("Test");
 
   @ArchTest
-  public static final ArchRule CLASSES_MAY_BE_IN_BASE_PACKAGE = classes()
-    .that().areNotAnnotatedWith(SpringBootApplication.class)
+  public static final ArchRule SPRING_CLASSES_MUST_BE_IN_THEIR_PACKAGE = classes()
+    .that().areAnnotatedWith(SpringBootApplication.class)
+    .or().areAnnotatedWith(Configuration.class)
     .should()
-    .resideInAnyPackage("*..(infra|application|domain)..*");
+    .resideInAnyPackage("*..spring.boot.poc.spring..");
+
+  @ArchTest
+  public static final ArchRule SPRING_ANNOTATIONS_MAY_NOT_BE_USED_IN_DOMAIN_OR_APP = classes()
+    .that().areAnnotatedWith(RestController.class)
+    .or().areAnnotatedWith(Controller.class)
+    .or().areAnnotatedWith(Service.class)
+    .should()
+    .resideOutsideOfPackage("*..(domain|application)..*");
 }
