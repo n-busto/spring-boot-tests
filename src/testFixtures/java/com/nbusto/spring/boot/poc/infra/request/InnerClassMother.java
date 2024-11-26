37d0d6633cfc87b5ca90bdfd875425f09c33a38c
n-busto <nicanor.manuel.busto@gmail.com>
2024-11-26 17:48:12 +0100

    Fix inner class mother

--- src/testFixtures/java/com/nbusto/spring/boot/poc/infra/request/InnerClassMother.java
+++ src/testFixtures/java/com/nbusto/spring/boot/poc/infra/request/InnerClassMother.java
@@ -10,18 +10,11 @@ import static org.instancio.Select.field;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class InnerClassMother {
 
-  public static InnerClass random() {
-    return of(InnerClass.class)
-      .generate(field(InnerClass::integer), it -> it.ints().min(0))
-      .generate(field(InnerClass::doubleValue), it -> it.doubles().min(0.0))
-      .create();
-  }
-
   public static InnerClass withNullString() {
     return of(InnerClass.class)
       .ignore(field(InnerClass::string))
       .generate(field(InnerClass::integer), it -> it.ints().min(0))
-      .generate(field(InnerClass::doubleValue), it -> it.doubles().min(0.0))
+      .generate(field(InnerClass::doubleValue), it -> it.doubles().max(-0.00001))
       .create();
   }
 
@@ -29,34 +22,34 @@ public class InnerClassMother {
     return of(InnerClass.class)
       .setBlank(field(InnerClass::string))
       .generate(field(InnerClass::integer), it -> it.ints().min(0))
-      .generate(field(InnerClass::doubleValue), it -> it.doubles().min(0.0))
+      .generate(field(InnerClass::doubleValue), it -> it.doubles().max(-0.00001))
       .create();
   }
 
   public static InnerClass withNullInteger() {
     return of(InnerClass.class)
       .ignore(field(InnerClass::integer))
-      .generate(field(InnerClass::doubleValue), it -> it.doubles().min(0.0))
+      .generate(field(InnerClass::doubleValue), it -> it.doubles().max(-0.00001))
       .create();
   }
 
   public static InnerClass withNegativeInteger() {
     return of(InnerClass.class)
       .generate(field(InnerClass::integer), it -> it.ints().max(0))
-      .generate(field(InnerClass::doubleValue), it -> it.doubles().min(0.0))
+      .generate(field(InnerClass::doubleValue), it -> it.doubles().max(-0.00001))
       .create();
   }
 
   public static InnerClass withNullDouble() {
     return of(InnerClass.class)
-      .generate(field(InnerClass::integer), it -> it.ints().max(0))
+      .generate(field(InnerClass::integer), it -> it.ints().min(0))
       .ignore(field(InnerClass::doubleValue))
       .create();
   }
 
   public static InnerClass withNegativeDouble() {
     return of(InnerClass.class)
-      .generate(field(InnerClass::integer), it -> it.ints().max(0))
+      .generate(field(InnerClass::integer), it -> it.ints().min(0))
       .generate(field(InnerClass::doubleValue), it -> it.doubles().max(0.0))
       .create();
   }
