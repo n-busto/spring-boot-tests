447fd71b96d6b9f9a10cd63d21171510f0e0497e
n-busto <nicanor.manuel.busto@gmail.com>
2024-11-27 10:13:32 +0100

    Fix ControllerTest annotation

--- src/testFixtures/java/com/nbusto/spring/boot/poc/infra/annotations/ControllerTest.java
+++ src/testFixtures/java/com/nbusto/spring/boot/poc/infra/annotations/ControllerTest.java
@@ -1,6 +1,5 @@
 package com.nbusto.spring.boot.poc.infra.annotations;
 
-import com.nbusto.spring.boot.poc.infra.status.controller.HealthCheckController;
 import com.nbusto.spring.boot.poc.spring.SpringBootTestsApplication;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.core.annotation.AliasFor;
@@ -11,7 +10,7 @@ import java.lang.annotation.*;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
-@WebMvcTest(controllers = HealthCheckController.class)
+@WebMvcTest
 @ContextConfiguration(classes = SpringBootTestsApplication.class)
 public @interface ControllerTest {
   @AliasFor(attribute = "controllers", annotation = WebMvcTest.class)
