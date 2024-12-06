22ac95d487f8ff14f8c3f0f78dbc44d83791f62e
nbusto <nicanor.manuel.busto@gmail.com>
2024-12-06 11:19:28 +0000

    Create basic validator

--- src/test/java/com/nbusto/spring/boot/poc/regresion/BaseKarateTest.java
+++ src/test/java/com/nbusto/spring/boot/poc/regresion/BaseKarateTest.java
@@ -5,7 +5,10 @@ import com.nbusto.spring.boot.poc.spring.SpringBootTestsApplication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.test.context.TestPropertySource;
+
+import java.io.IOException;
 
 @SpringBootTest(
   webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
@@ -21,10 +24,14 @@ public abstract class BaseKarateTest {
   @Autowired
   private ServletWebServerApplicationContext webContext;
 
+  @Autowired
+  private ResourceLoader resourceLoader;
+
   @Karate.Test
-  Karate testFeature() {
+  Karate testFeature() throws IOException {
     return Karate.run("classpath:karate/scenarios/" + getFeatureName())
       .failWhenNoScenariosFound(true)
+      .hook(new OpenApiValidatorHook(resourceLoader))
       .systemProperty("karate.server.port", webContext.getWebServer().getPort() + "");
   }
 
