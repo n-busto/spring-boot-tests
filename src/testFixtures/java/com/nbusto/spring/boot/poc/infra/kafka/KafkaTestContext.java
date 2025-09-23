4165299e48641763daf7867570a9835de365e50b
nbusto <nicanor.manuel.busto@gmail.com>
2025-09-23 13:51:11 +0200

    Use constant for cp version

--- src/testFixtures/java/com/nbusto/spring/boot/poc/infra/kafka/KafkaTestContext.java
+++ src/testFixtures/java/com/nbusto/spring/boot/poc/infra/kafka/KafkaTestContext.java
@@ -2,13 +2,13 @@ package com.nbusto.spring.boot.poc.infra.kafka;
 
 import com.nbusto.spring.boot.poc.spring.SpringBootTestsApplication;
 import io.confluent.kafka.serializers.KafkaAvroDeserializer;
-import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
@@ -17,6 +17,7 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.kafka.ConfluentKafkaContainer;
 
+import java.util.Collections;
 import java.util.Map;
 
 import static org.testcontainers.utility.DockerImageName.parse;
@@ -25,18 +26,19 @@ import static org.testcontainers.utility.DockerImageName.parse;
 @SpringBootTest(classes = SpringBootTestsApplication.class)
 public abstract class KafkaTestContext {
 
+  public static final String CONFLUENT_PLATFORM_VERSION = "7.6.5";
+
   private static final Network NETWORK = Network.newNetwork();
 
   @Container
   static ConfluentKafkaContainer KAFKA_CONTAINER =
-    new ConfluentKafkaContainer(parse("confluentinc/cp-kafka:7.6.5"))
-      .withExposedPorts(9092)
+    new ConfluentKafkaContainer(parse("confluentinc/cp-kafka:" + CONFLUENT_PLATFORM_VERSION))
       .withNetworkAliases("kafka")
       .withNetwork(NETWORK);
 
   @Container
   static GenericContainer<?> SCHEMA_REGISTRY_CONTAINER =
-    new GenericContainer<>(parse("confluentinc/cp-schema-registry:7.6.5"))
+    new GenericContainer<>(parse("confluentinc/cp-schema-registry:" + CONFLUENT_PLATFORM_VERSION))
       .withNetwork(NETWORK)
       .withExposedPorts(8081)
       .withNetworkAliases("schema-registry")
@@ -53,26 +55,30 @@ public abstract class KafkaTestContext {
       KAFKA_CONTAINER::getBootstrapServers);
   }
 
-  static Map<String, Object> consumerProps() {
-    return Map.of("schema.registry.url", buildSchemaRegistryServerUri(),
-      ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_CONTAINER.getBootstrapServers(),
-      ConsumerConfig.GROUP_ID_CONFIG, "testConsumer",
-      ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true",
-      ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "10",
-      ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "60000",
-      ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
-      ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class,
-      ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest",
-      KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG, "true");
+  private static Map<String, Object> consumerProps() {
+    final var properties = KafkaTestUtils.consumerProps(
+      KAFKA_CONTAINER.getBootstrapServers(),
+      "test-group",
+      "true"
+    );
+
+    properties.put("schema.registry.url", buildSchemaRegistryServerUri());
+    properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+    properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class);
+
+    return properties;
   }
 
   private static @NotNull String buildSchemaRegistryServerUri() {
     return "http://localhost:" + SCHEMA_REGISTRY_CONTAINER.getMappedPort(8081);
   }
 
-  protected <T> Consumer<String, T> createConsumer() {
-    final var consumerFactory = new DefaultKafkaConsumerFactory<String, T>(consumerProps());
+  protected static <T> Consumer<String, T> createConsumer(final String topic) {
+    final var consumer = new DefaultKafkaConsumerFactory<String, T>(consumerProps())
+      .createConsumer();
 
-    return consumerFactory.createConsumer();
+    consumer.subscribe(Collections.singletonList(topic));
+
+    return consumer;
   }
 }
