4e51b40492105a1e9a66f69a4fe690813806f2e2
nbusto <nicanor.manuel.busto@gmail.com>
2025-09-26 00:56:22 +0200

    Create rest controller for kafka event delete

---
+++ src/main/java/com/nbusto/spring/boot/poc/infra/kafka/controller/OrderDeleteRestController.java
@@ -0,0 +1,33 @@
+package com.nbusto.spring.boot.poc.infra.kafka.controller;
+
+import com.nbusto.spring.boot.poc.domain.kafka.Order;
+import com.nbusto.spring.boot.poc.infra.kafka.producer.OrderDeleteEventProducer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.OffsetDateTime;
+
+@RestController
+@RequestMapping("/injection/kafka/order/{id}")
+@RequiredArgsConstructor
+public class OrderDeleteRestController {
+
+  private final OrderDeleteEventProducer orderDeleteEventProducer;
+
+  public ResponseEntity<Void> deleteOrder(
+    @PathVariable final String id
+  ) {
+    orderDeleteEventProducer.sendDeleteEvent(createOrder(id));
+
+    return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
+  }
+
+  private Order createOrder(final String id) {
+    return new Order(id, null, null, OffsetDateTime.now());
+  }
+
+}
