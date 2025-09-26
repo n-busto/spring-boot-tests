c485404764a6d27551e01d89f0ee8ed875fe4431
nbusto <nicanor.manuel.busto@gmail.com>
2025-09-26 04:12:00 +0200

    remove not used property

--- src/main/java/com/nbusto/spring/boot/poc/spring/kafka/KafkaProperties.java
+++ src/main/java/com/nbusto/spring/boot/poc/spring/kafka/KafkaProperties.java
@@ -4,7 +4,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties("app.config.kafka")
 public record KafkaProperties(
-  String groupId,
   Topics topics
 ) {
 
